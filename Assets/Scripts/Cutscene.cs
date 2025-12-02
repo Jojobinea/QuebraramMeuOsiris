080869699ade07b8fafd234f2308a9a7ba0fc71d
Juliene Witória Viana <julienewitoriaaa@gmail.com>
2025-12-02 17:54:01 -0400

    cutscene

---
+++ Assets/Scripts/Cutscene.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Cutscene : MonoBehaviour
+{
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
